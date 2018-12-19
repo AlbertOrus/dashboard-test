52f6ea7da456dfa8081a0a831f5142b4d72ffa4d
Albert <albertroid96@gmail.com>
2018-12-19 18:06:37 +0100

    Mejoras en table

--- src/app/state-table/state-table.component.ts
+++ src/app/state-table/state-table.component.ts
@@ -7,13 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StateTableComponent implements OnInit {
   
+  private bad_latency = 100;
+  private normal_latency = 50;
+
   public hospitals: any[] = [
-  {nom: 'GXX', estat: 'Correcte', latencia: 30, n_visites: 38},
-  {nom: 'BVX', estat: 'Lent', latencia: 200, n_visites: 54},
-  {nom: 'HBB', estat: 'Correcte', latencia: 45, n_visites: 70},
-  {nom: 'THN', estat: 'Correcte', latencia: 60, n_visites: 30}
+  {nom: 'GXX', estat: '', latencia: this.getLatency(), n_visites: 38, class: ""},
+  {nom: 'BVX', estat: '', latencia: this.getLatency(), n_visites: 54, class: ""},
+  {nom: 'HBB', estat: '', latencia: this.getLatency(), n_visites: 70, class: ""},
+  {nom: 'THN', estat: '', latencia: this.getLatency(), n_visites: 30, class: ""}
   ]
 
+  private getLatency():String{
+  	return Number(Math.random()*100).toFixed(2);
+  }
 
   public getHospitals():String{
   	return '';
@@ -22,6 +28,23 @@ export class StateTableComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  	for (let hospital of this.hospitals) {
+  		if(hospital.latencia > this.bad_latency){
+  			hospital.estat = "Lent";
+
+  			
+  		}else if(hospital.latencia > this.normal_latency){
+  			hospital.estat ="Normal";
+  			
+  		}
+  		else{
+  			hospital.estat ="Rapid";
+  			
+  		}
+
+  		
+  	}
   }
+  
 
 }
